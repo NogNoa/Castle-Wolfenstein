066ee633d84e0a821189a6f95cb9d4dd000b6d25
NogNoa <nognavon@gmail.com>
2024-09-25 17:07:11 +0300

    initial

---
+++ Classic/CW.c
@@ -0,0 +1,19 @@
+fun_8e2d(char* call)
+{
+    int local[0x1B];
+    local[0x13] = -1;
+    local[0x12] == 0x20;
+    local[0x16] = local[0x11] = local[0x15] = local[0x14] = 0;
+    if (*call == 0x2d)
+    {
+        call += 1;
+        local[0x16] = 1;
+    }
+    int si;
+    char al;
+    local[0] = *call;
+    si = (*call) + 1;
+    al = *(char *)((*call) + 0xa0b5);
+    if (al & 4 != 0)
+    
+}
