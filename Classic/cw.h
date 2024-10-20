cd7c7d9b373fb22fd39d9772e167d1d8f69b54b9
NogNoa <nognavon@gmail.com>
2024-10-20 11:09:40 +0300

    sum

--- Classic/cw.h
+++ Classic/cw.h
@@ -1,2 +1,4 @@
 typedef char byte;
 typedef char* string;
+
+int sum(int *list, int len);
---
+++ Classic/helper.c
@@ -0,0 +1,5 @@
+int sum(int *list, int len)
+{
+    int back = 0;
+    for (;len > 0; --len, ++list) {back += *list;}
+}
