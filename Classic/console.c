8212b016f6fa1b032b3cb52d03a8c186da09f7b7
NogNoa <nognavon@gmail.com>
2024-10-26 14:01:01 +0300

    gcc check the rest

--- Classic/console.c
+++ Classic/console.c
@@ -7,8 +7,8 @@ extern bool pcjr;
 put_2_lines(line0, line1)
 char *line0, *line1;
 {   
-    if (pcjr) {set_video_mode(TXT_GREY_WD);}
-    else {set_video_mode(TXT_GREY_THN);}
+    if (pcjr) {setVideoMode(TXT_GREY_WD);}
+    else {setVideoMode(TXT_GREY_THN);}
     BiosVideo(SET_CURSOR_POSITION, 0, 0, RowColl(4,1));
     cputs(line0);
     cputs(line1);
--- Modern/cw.h
+++ Modern/cw.h
@@ -1,5 +1,24 @@
-typedef char byte;
-typedef char* string;
+#ifndef PROG_CW
+#define PROG_CW
+
+typedef char * string;
 typedef unsigned int uint;
 
+typedef char bool;
+enum {false, true};
+
+#ifndef LATTICE
+#ifndef __STDC__
+typedef char byte;
+#else
+#include "stdbool.h"
+#endif
+#endif
+
+#ifndef NULL
+#define NULL 0
+#endif
+
 int sum(int *list, int len);
+
+#endif
