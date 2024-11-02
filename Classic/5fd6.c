ef2aa7ad428a556b68df30f65cf2ee61a757facd
NogNoa <nognavon@gmail.com>
2024-11-02 16:03:52 +0200

    supposedly redundent variables

--- Classic/5fd6.c
+++ Classic/5fd6.c
@@ -11,45 +11,30 @@ del_inst_5ff9(void)
 
 int f5fd6(int p1, void* p2)
 {
-    int b, ax;
-    byte al, ah, cl, ch, dl, dh;
+    int b, a;
+    byte   i;
     byte * si;
     int i5ff0 = 0;
-    ch = (byte) p1;
-    cl = 0x14;
-    al = 1;
-    ah = 2;
-    b = p2;
-    i5ff0 = b;
-    dl = *default_drive;
-    b = i5ff0;
-    dh = 0;
+    i5ff0 = b = p2;
     switch(i5ff9)
     {case 0x1000:
-        *(si + b) = dl;
+        *(si + b) = *default_drive;
         break;
     case 0x9090:
         break;
     case 0x200:
-        *(si + b) = al;
+        *(si + b) = 1;
         break;
     }
-    al = 0;
-    ax = ah << 8 + al;
-    i5ff9 = ax;
-    cl = 28; ch = 0;
-    ax = 0;
-    b = i5ff0;
-    si = 0x10;
-    while (cl--)
-    {   ax  |= *(si + b);
-        si += 2;   
-    }
+    a = 0x200;
+    i5ff9 = a;
+    for (a = 0, si= 0x10, i=28;
+         i;
+            a |= *(si + i5ff0),
+            si +=2,
+            i--)
     i5ff9 = 0x1000;
-    ax = 0xa5a5 | 0xa5a5;
-    if (!ax) 
-    {   ax = -1;
-        return 0;
-    }
-    else    {return ax;}
+    a = 0xa5a5 | 0xa5a5;
+    if (!a) {return 0;}
+    else    {return a;}
 }
