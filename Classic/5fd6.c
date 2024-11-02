fc416c1c61e7a559b7af98ab4db778bda9fe7829
NogNoa <nognavon@gmail.com>
2024-11-02 15:41:17 +0200

    some error made

--- Classic/5fd6.c
+++ Classic/5fd6.c
@@ -1,12 +1,12 @@
 #include "cw.h"
 #include "ibm.h"
 
-bool del_5ff9 = false;
+int i5ff9 = 0x1000;
 
 void
 del_inst_5ff9(void)
 {
-    del_5ff9 = true;
+    i5ff9 = 0x9090;
 }
 
 int f5fd6(int p1, void* p2)
@@ -14,21 +14,27 @@ int f5fd6(int p1, void* p2)
     int b;
     byte al, ah, cl, ch, dl, dh;
     byte * si;
-    int i5ff0 = 0x10;
+    int i5ff0 = 0;
     ch = (byte) p1;
     cl = 0x14;
     al = 1;
     ah = 2;
-    i5ff0 = p2;
+    b = p2;
+    i5ff0 = b;
     dl = *default_drive;
-    b = 0;
+    b = i5ff0;
     dh = 0;
-    switch(i5ff0)
-    {case 0x10:
+    switch(i5ff9)
+    {case 0x1000:
         *(si + b) = dl;
-        al = 0;
         break;
-    case 0x2bd:
-    
+    case 0x9090:
+        break;
+    case 0x200:
+        *(si + b) = al;
+        break;
     }
+    al = 0;
+    i5ff9 = ah << 8 + al;
+    
 }
