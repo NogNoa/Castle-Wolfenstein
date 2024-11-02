7c72ba2b0f963fc4d9e9d08d2431bdaa6713f1a9
NogNoa <nognavon@gmail.com>
2024-11-02 15:53:53 +0200

    finish manually decompile

--- Classic/5fd6.c
+++ Classic/5fd6.c
@@ -11,7 +11,7 @@ del_inst_5ff9(void)
 
 int f5fd6(int p1, void* p2)
 {
-    int b;
+    int b, ax;
     byte al, ah, cl, ch, dl, dh;
     byte * si;
     int i5ff0 = 0;
@@ -35,6 +35,21 @@ int f5fd6(int p1, void* p2)
         break;
     }
     al = 0;
-    i5ff9 = ah << 8 + al;
-    
+    ax = ah << 8 + al;
+    i5ff9 = ax;
+    cl = 28; ch = 0;
+    ax = 0;
+    b = i5ff0;
+    si = 0x10;
+    while (cl--)
+    {   ax  |= *(si + b);
+        si += 2;   
+    }
+    i5ff9 = 0x1000;
+    ax = 0xa5a5 | 0xa5a5;
+    if (!ax) 
+    {   ax = -1;
+        return 0;
+    }
+    else    {return ax;}
 }
