162eba79f22362ae51b6049390ac931d7deb8e0f
NogNoa <nognavon@gmail.com>
2024-11-28 19:04:26 +0200

    shifting a long is hard

--- src/memory.h
+++ src/memory.h
@@ -11,11 +11,11 @@ void fixit(void);
 
 #define long_ptr(S, I) ((long)S << 0x10 | I)
 
-#define SegMemSet(A, b)    (SegmSt((int) ((A) >> 0x10), (char *) (A), (byte) (b)))
+#define SegMemSet(A, b)    (SegmSt((int) ((A) / 0x100), (char *) (A), (byte) (b)))
 #define WSegMem_Set(A, w)  ((SegMemSet((long) A, w)),   (SegMemSet((long) A+1, w >> 8)))
 #define LSeg_Mem_Set(A, l) ((WSegMem_Set(A, l)), (WSegMem_Set(A+2, l >> 0x10)))
 
-#define SegMemGet(A)       (SegmGt((int) ((A) >> 0x10), (char *) (A)))
+#define SegMemGet(A)       (SegmGt((int) ((A) / 0x100), (char *) (A)))
 #define WSegMem_Get(A)  ((SegMemGet((long)A)) | (SegMemGet((long) A+1)) << 8)
 #define LSeg_Mem_Get(A) ((WSegMem_Get(A)) | (WSegMem_Get(A+2)) << 0x10)
 
