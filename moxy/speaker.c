fac7a2c009eed6c1157c774c0d3da406afb36de6
NogNoa <53447424+NogNoa@users.noreply.github.com>
2025-08-06 14:09:04 +0300

    macro

--- moxy/speaker.c
+++ moxy/speaker.c
@@ -5,16 +5,17 @@
 long timer_count;
 byte carousel_count;
 
+#define ROR_WORD(X) ((X) >> 1) | (((X) & 1)<< 0xf)
+
 byte f03a1(byte ah)
 {   
     byte al;
-    int l7712, l7714, b;
+    int l7712, l7714;
     al = (isDemo) ? *(++GfxFileP) : (byte) timer_count;
     al >>= 1;
-    l7712 = ((l7712) >> 1) | (((l7712) & 1)<< 0xf);
-    l7714 = ((l7714) >> 1) | (((l7714) & 1)<< 0xf);
-    l7714 |= cf1 << 0xf;
-    l7714 = (((int) ah << 8) | al) ^ ((l7714 >> 1) | ((l7714) & 1) << 0xf);
+    l7712 = ROR_WORD(l7712);
+    l7714 = ROR_WORD(l7714);
+    l7714 = (((int) ah << 8) | al) ^ ROR_WORD(l7714);
     al = (byte) l7714;
     if (al < carousel_count)
         {pause_resume_sound();}
