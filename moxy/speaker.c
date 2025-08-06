f7b6f59df85eb095025c2596cca9ac4401d85f9b
NogNoa <53447424+NogNoa@users.noreply.github.com>
2025-08-06 13:41:03 +0300

    get rid of a and b

--- moxy/speaker.c
+++ moxy/speaker.c
@@ -3,26 +3,24 @@
 #include "files.h"
 
 long timer_count;
-int carousel_count;
+byte carousel_count;
 
 byte f03a1(byte ah)
 {   
     byte al, cf0, cf1,;
-    int l7712, l7714, b, a;
+    int l7712, l7714, b;
     al = (isDemo) ? *(++GfxFileP) : (byte) timer_count;
     cf0 = al & 1;
     cf1 = l7712 & 1;
     l7712 >>= 1;
-    l7712 |= cf0 << 7;
+    l7712 |= cf0 << 0xf;
+    cf0 = l7714 & 1;
     l7714 >>= 1;
-    l7714 |= cf1 << 7;
-    b = l7714;
-    b >>= 1;
-    a = ((int) ah << 8) | al;
-    a ^= b;
-    l7714 = a;
-    ah = 0;
+    l7714 |= cf1 << 0xf;
+    cf1 = l7714 & 1;
+    l7714 = (((int) ah << 8) | al) ^ ((l7714 >> 1) | (cf0 << 0xf));
+    al = (byte) l7714;
     if (al < carousel_count)
         {pause_resume_sound();}
-    return (byte) a;
+    return al;
 }
