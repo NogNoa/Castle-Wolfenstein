3838adad8cb3d16a9f7200bee0ef6a4e8103a3b2
NogNoa <53447424+NogNoa@users.noreply.github.com>
2025-08-06 13:51:30 +0300

    rotate without carry

--- moxy/speaker.c
+++ moxy/speaker.c
@@ -7,18 +7,14 @@ byte carousel_count;
 
 byte f03a1(byte ah)
 {   
-    byte al, cf0, cf1,;
+    byte al;
     int l7712, l7714, b;
     al = (isDemo) ? *(++GfxFileP) : (byte) timer_count;
-    cf0 = al & 1;
-    cf1 = l7712 & 1;
-    l7712 >>= 1;
-    l7712 |= cf0 << 0xf;
-    cf0 = l7714 & 1;
-    l7714 >>= 1;
+    al >>= 1;
+    l7712 = ((l7712) >> 1) | (((l7712) & 1)<< 0xf);
+    l7714 = ((l7714) >> 1) | (((l7714) & 1)<< 0xf);
     l7714 |= cf1 << 0xf;
-    cf1 = l7714 & 1;
-    l7714 = (((int) ah << 8) | al) ^ ((l7714 >> 1) | (cf0 << 0xf));
+    l7714 = (((int) ah << 8) | al) ^ ((l7714 >> 1) | ((l7714) & 1) << 0xf);
     al = (byte) l7714;
     if (al < carousel_count)
         {pause_resume_sound();}
