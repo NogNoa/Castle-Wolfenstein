a9c7f6a54b72345aae55770d865a01014eede5a8
NogNoa <53447424+NogNoa@users.noreply.github.com>
2025-08-10 13:24:53 +0300

    f45e9

--- src/quort.c
+++ src/quort.c
@@ -59,3 +59,39 @@ f44db(void)
         }
     }
 }
+
+void f45e9(void)
+{
+  byte (*fb_midpg)[0x10]; /* 8 rows */
+  byte spk, nibble_j, carry_over, j;
+  int pg, i;
+  
+  carry_over = 0xf; 
+  j = 0;
+  for (pg = 1; pg < 0x3f; pg = pg + 1) {
+    fb_midpg = (byte (*)[0x10]) (file_buffer + PAGE_SZ*pg + PAGE_SZ/2);
+    for (i = 0; i <= 0x7; i = i + 0x1) {
+      if ((fb_midpg[i][0] & 0xf0) == 0x30) {
+        /* at first the nibble goes up to f
+        but then it's 0 twice in a row instead*/
+        nibble_j = j & 0xf;
+        if (nibble_j == 0xf) {
+          nibble_j = carry_over;
+          carry_over = 0;
+        }
+        fb_midpg[i][2] = nibble_j;
+        spk = (byte) SpkRng();
+        f4910(spk);
+        fb_midpg[i][4] = spk;
+        spk = (byte) SpkRng();
+        fb_midpg[i][3] = spk;
+        fb_midpg[i][7] = 0;
+        j = j + 1;
+      }
+      else if ((fb_midpg[i][0] & 0xf0) == 0x50) {
+        fb_midpg[i][3] = 1;
+      }
+    }
+  }
+  return;
+}
