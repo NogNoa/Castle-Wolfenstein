e6d1893a3903ce49720176157870b72a206bfed0
NogNoa <53447424+NogNoa@users.noreply.github.com>
2025-09-13 19:47:03 +0300

    reorder endays

--- src/endays.c
+++ src/endays.c
@@ -4,6 +4,19 @@
 int i9ec0, i9ec2;
 byte a9ea0[0x20];
 
+void 
+fun728c()
+{
+    while (get_9ea0());
+}
+
+
+byte
+get_9ea0()
+{
+    return olget9ea0();
+}
+
 byte
 olget9ea0()
 {
@@ -16,19 +29,6 @@ olget9ea0()
     a9ea0[i9ec0] = 0;
     i9ec0 = (i9ec0 + 1) % 0x20;
     return old;
-}
-
-
-byte
-get_9ea0()
-{
-    return olget9ea0();
-}
-
-void 
-fun728c()
-{
-    while (get_9ea0());
 }
 
 
