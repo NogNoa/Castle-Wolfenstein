bb8fff1982c5fc16b6a5c4b86c8b062c8474338b
NogNoa <53447424+NogNoa@users.noreply.github.com>
2025-10-27 21:19:05 +0200

    special cases

--- utilities/wolf_string_ext.py
+++ utilities/wolf_string_ext.py
@@ -18,7 +18,12 @@ with open("./resources/CW.exe", "rb") as file:
 
 while scroll:
     offset = strings_end - len(scroll)
-    string, scroll = scroll.split(b'\0',1)
+    if offset in (0x90d3, 0x90d4):
+        string, scroll = scroll[:1], scroll[1:]
+    elif offset == 0x9124:
+        string, scroll = scroll[:2], scroll[2:]
+    else:
+        string, scroll = scroll.split(b'\0',1)
     string = CGA.draw_w_costume_font(string, font)
     string.save("../Media/%x.png" % offset)
     scroll = scroll.lstrip(b'\0')
