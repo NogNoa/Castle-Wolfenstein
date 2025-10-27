4ee715549c169b07dcca5eb230ed85e78bb8b94a
NogNoa <53447424+NogNoa@users.noreply.github.com>
2025-10-27 21:24:36 +0200

    merge two offset vars

--- utilities/wolf_string_ext.py
+++ utilities/wolf_string_ext.py
@@ -6,7 +6,7 @@ os.chdir("utilities")
 DATA_SEGMENT_in_FILE = 0xA470
 
 strings_offset = 0x907d
-strings_end = 0x9164
+STRINGS_END = 0x9164
 
 with open("../bin/WOLF.CHR", "rb") as file:
     font = file.read()
@@ -14,16 +14,16 @@ font = CGA.get_1bit_font(font)
 
 with open("./resources/CW.exe", "rb") as file:
     file.seek(strings_offset + DATA_SEGMENT_in_FILE)
-    scroll = file.read(strings_end - strings_offset)
+    scroll = file.read(STRINGS_END - strings_offset)
 
 while scroll:
-    offset = strings_end - len(scroll)
-    if offset in (0x90d3, 0x90d4):
+    if strings_offset in (0x90d3, 0x90d4):
         string, scroll = scroll[:1], scroll[1:]
-    elif offset == 0x9124:
+    elif strings_offset == 0x9124:
         string, scroll = scroll[:2], scroll[2:]
     else:
         string, scroll = scroll.split(b'\0',1)
     string = CGA.draw_w_costume_font(string, font)
-    string.save("../Media/%x.png" % offset)
+    string.save("../Media/%x.png" % strings_offset)
     scroll = scroll.lstrip(b'\0')
+    strings_offset = STRINGS_END - len(scroll)
