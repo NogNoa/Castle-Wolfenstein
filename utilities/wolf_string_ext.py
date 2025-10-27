98ffddeeace62b115c929483d75bf44a39b61efc
NogNoa <53447424+NogNoa@users.noreply.github.com>
2025-10-27 19:01:39 +0200

    go into the directory
    lstrip properly and lastly

--- utilities/wolf_string_ext.py
+++ utilities/wolf_string_ext.py
@@ -1,22 +1,24 @@
 import CGA
-CGAROM = "utilities/resources/IBM-CGA.rom"
+import os
+
+os.chdir("utilities")
 
 DATA_SEGMENT_in_FILE = 0xA470
 
 strings_offset = 0x907d
 strings_end = 0x9164
 
-with open("bin/WOLF.CHR", "rb") as file:
+with open("../bin/WOLF.CHR", "rb") as file:
     font = file.read()
 font = CGA.get_1bit_font(font)
 
-with open("utilities/resources/CW.exe", "rb") as file:
+with open("./resources/CW.exe", "rb") as file:
     file.seek(strings_offset + DATA_SEGMENT_in_FILE)
     scroll = file.read(strings_end - strings_offset)
 
 while scroll:
-    scroll.lstrip(b'\0')
     offset = strings_end - len(scroll)
     string, scroll = scroll.split(b'\0',1)
     string = CGA.draw_w_costume_font(string, font)
-    string.save("%x.png" % offset)
+    string.save("../Media/%x.png" % offset)
+    scroll = scroll.lstrip(b'\0')
